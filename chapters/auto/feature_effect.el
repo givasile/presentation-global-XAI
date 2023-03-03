1023a638910cc730e470deb949db382439855d30
Vasilis Gkolemis <vgkolemis@athenarc.gr>
2023-03-03 13:04:20 +0200

    second chapter completed

---
+++ chapters/auto/feature_effect.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook
+ "feature_effect"
+ (lambda ()
+   (TeX-run-style-hooks
+    "figures/chapter2/ovf_1_reality"
+    "figures/chapter2/ovf_2_sampling"
+    "figures/chapter2/ovf_3_linear"
+    "figures/chapter2/ovf_4_quadratic"
+    "figures/chapter2/ovf_5_3d"
+    "figures/chapter2/ovf_6_9d"))
+ :latex)
+
